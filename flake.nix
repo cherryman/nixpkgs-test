bffa976bf5198a6dc0e68ef78ec959849c657bc8
Sheheryar Parvaz <me@cherryman.org>
2025-02-16 09:54:57 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "testing nixpkgs on ci on all systems";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          packages.default = pkgs.hello;
+        };
+      flake = { };
+    };
+}
